0fd25a678fcdb3cfc0bb7024bd9887e816f93438
abelladius <szolosi.albert15@gmail.com>
2023-10-27 19:17:24 +0300

    Starting files
    This version highlights the initial layout, styling, and content structure. However, there's more to come!

---
+++ index.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for nav
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scroll({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
